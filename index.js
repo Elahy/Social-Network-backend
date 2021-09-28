f6f8ccbf6f0150236d1acf8c2e6b6ecb1b555189
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-09-28 18:22:19 +0600

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(console.log("connected to DB"));
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/users", userRoute);
+app.use("/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Backend is running!");
+});
